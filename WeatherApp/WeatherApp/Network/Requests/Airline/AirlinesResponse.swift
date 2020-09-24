1968e222bae98fc6cf4815a5c16e3a5dcea69d04
Dzmitry Pliushchai <pliushchai@rushstreetinteractive.com>
2020-09-24 10:47:41 +0300

    initial commit

---
+++ WeatherApp/WeatherApp/Network/Requests/Airline/AirlinesResponse.swift
@@ -0,0 +1,12 @@
+//
+//  AirlinesResponse.swift
+//  WeatherApp
+//
+//  Created by Dmitry Pliushchai on 11/27/19.
+//  Copyright © 2019 Dmitry. All rights reserved.
+//
+
+struct AirlineResponse: Decodable {
+    let name: String
+    let code: String
+}
